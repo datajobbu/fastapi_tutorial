39a4d7f5baf2e45c814e70e521df56764367e1ff
hojuly <hojune.choi.me@gmail.com>
2021-04-05 15:38:27 +0900

    Query Parameters

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"},
+                 {"item_name": "Bar"},
+                 {"item_name": "Baz"}]
+
 class Item(BaseModel):
     name: str
     price: float
@@ -24,13 +28,39 @@ async def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
 @app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Optional[str] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
 
 @app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
+def update_item(item_id: str, item: Item):
     return {"item_name": item.name, "item_id": item_id}
 
 
@@ -40,8 +70,11 @@ async def read_user_me():
 
 
 @app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
+async def read_user(
+    user_id: str, needy: str, skip: int = 0, limit: Optional[int] = None
+):
+    user = {"user_id": user_id, "needy": needy, "skip": skip, "limit": limit}
+    return user
 
 
 @app.get("/models/{model_name}")
